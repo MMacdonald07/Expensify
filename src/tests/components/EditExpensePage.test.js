72717a0233599ef2950bc098627a8f3a1e4af1d8
mdmac <matthew.mmacdonald@gmail.com>
2020-10-25 12:42:17 +0000

    Setup startRemoveExpense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,27 +3,34 @@ import { shallow } from 'enzyme';
 import { EditExpensePage } from '../../components/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let editExpenseSpy, removeExpenseSpy, historySpy, wrapper;
+let editExpenseSpy, startRemoveExpenseSpy, historySpy, wrapper;
 
 beforeEach(() => {
-    editExpenseSpy = jest.fn();
-    removeExpenseSpy = jest.fn();
-    historySpy = { push: jest.fn() };
-    wrapper = shallow(<EditExpensePage editExpense={editExpenseSpy} history={historySpy} removeExpense={removeExpenseSpy} expense={expenses[2]} />);
+	editExpenseSpy = jest.fn();
+	startRemoveExpenseSpy = jest.fn();
+	historySpy = { push: jest.fn() };
+	wrapper = shallow(
+		<EditExpensePage
+			editExpense={editExpenseSpy}
+			history={historySpy}
+			startRemoveExpense={startRemoveExpenseSpy}
+			expense={expenses[2]}
+		/>
+	);
 });
 
 test('Should render EditExpensePage correctly', () => {
-    expect(wrapper).toMatchSnapshot();
+	expect(wrapper).toMatchSnapshot();
 });
 
 test('Should handle onSubmit', () => {
-    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
-    expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(editExpenseSpy).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
+	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+	expect(historySpy.push).toHaveBeenLastCalledWith('/');
+	expect(editExpenseSpy).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
 
 test('Should handle removeExpense', () => {
-    wrapper.find('button').simulate('click');
-    expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(removeExpenseSpy).toHaveBeenLastCalledWith({ id: expenses[2].id });
+	wrapper.find('button').simulate('click');
+	expect(historySpy.push).toHaveBeenLastCalledWith('/');
+	expect(startRemoveExpenseSpy).toHaveBeenLastCalledWith({ id: expenses[2].id });
 });
